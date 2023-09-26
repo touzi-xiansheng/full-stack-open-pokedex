c4dc18d5749ece5eef3303811a4c5ab7f4277ac6
tzz <touzi_xiansheng@outlook.com>
2023-09-26 14:39:53 +0800

    fix eslint

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+
+import { createRoot } from 'react-dom/client'
+const container = document.getElementById('app')
+const root = createRoot(container)
+root.render(<App />)
